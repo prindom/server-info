770f277a5d4ac9cde5d73aec200400ab854dfcbf
dprinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2017-12-21 12:36:19 +0100

    updates

--- index.php
+++ index.php
@@ -8,15 +8,41 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body style="font-family: Raleway, sans-serif">
-<div style="display: flex; justify-content: space-between; align-items: center">
-    <h1>Here are our sites -- test --</h1>
-    <a href="/phpmyadmin">PHPMYADMIN</a>
-    <div onclick="goBack()" onmouseenter="goBackHover()" style="display: flex; justify-content: space-between; align-items: center; cursor: pointer" data-toggle="tooltip" data-placement="left" title="there is no going back">
-        <i class="fa fa-chevron-circle-left fa-2x" aria-hidden="true"></i>
-        <p>Go back</p>
-    </div>
 
-</div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src="assets/imgs/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+      Bootstrap
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/phpmyadmin">phpmyadmin</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
 	<?php
 		echo "";
