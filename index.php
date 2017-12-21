f01fdaf4ae6886e2c44c13132563285b6ed75f63
dprinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2017-12-21 12:38:28 +0100

    update

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 <html>
 <head>
 	<title>HGB1 server</title>
-	<link href="../hgb1.png" rel="shortcut icon">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300i,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link href="../hgb1.png" rel="shortcut icon">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300i,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/css/custom.css" />
 </head>
 <body style="font-family: Raleway, sans-serif">
 
@@ -77,32 +82,9 @@
 		echo $li;
 		closedir($dh);
 	?>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script>
-    $(document).ready(function(){
-        //$('[data-toggle="tooltip"]').tooltip();
-        let array = $("td");
-        //console.log(array);
-        for (let i = 0; i < array.length; i++) {
-            //console.log($(array[i]));
-            if($(array[i]).text() == "website/directory") {
-                $(array[i]).parent().addClass("success");
-            } else if ($(array[i]).text() == "file"){
-                $(array[i]).parent().addClass("info");
-            }
-        }
-    });
-
-    function goBackHover() {
-        $('[data-toggle="tooltip"]').tooltip();
-    }
-
-    function goBack() {
-        if (document.URL == "http://hgb1.damnserver.com:81/") {
-            console.log("do nothing");
-        }else window.history.back();
-    }
-</script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery-3.2.1.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/code.js"></script>
 </body>
 </html>
