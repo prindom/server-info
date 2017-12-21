f053c0b78b4ceff941f49696398ecec572058ee5
dprinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2017-12-21 13:19:35 +0100

    update

--- index.php
+++ index.php
@@ -49,12 +49,12 @@
     </div>
   </nav>
 
-  <div class="">
+  <div class="ssss">
     <?php
   		echo "";
   		$path = ".";
   		$dh = opendir($path);
-  		$li = "<table class='table table-responsive'><thead class='thead-dark'><tr><th>Title</th><th>Link</th><th>File Type</th><th>Last time modified</th></tr></thead><tbody>";
+  		$li = "<table class='table table-responsive'><thead class='thead-dark'><tr><th>Title</th><th>Operations</th><th>File Type</th><th>Last time modified</th></tr></thead><tbody>";
   		$aList = array();
   		$sEntrie = "";
   		$i=1;
@@ -69,7 +69,7 @@
   		while (($file = readdir($dh)) !== false) {
       	if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
 
-              $sEntrie = "<tr><td>".ucfirst($file)."</td><td><a href='$path/$file'>hgb1.damnserver.com/$file</a></td><td>".getFileType(filetype($file))."</td><td>".date ("F d Y H:i:s.", filemtime($file))."</td></tr>";
+              $sEntrie = "<tr><td>".ucfirst($file)."</td><td><i class='fa fa-external-link' aria-hidden='true'><a href='$path/$file'></a></i></td><td>".getFileType(filetype($file))."</td><td>".date ("F d Y H:i:s.", filemtime($file))."</td></tr>";
               array_push($aList, $sEntrie);
               //$li.= "<tr><td>".ucfirst($file)."</td><td>Dominik Prinzensteiner</td><td><a href='$path/$file'>hgb1.damnserver.com/$file</a></td><td>".date ("F d Y H:i:s.", filemtime($file))."</td></tr>";
   				$i++;
